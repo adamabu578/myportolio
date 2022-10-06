88dacc87f177bcfde2dd1d499494cd603698f82f
adamabu578 <107815562+adamabu578@users.noreply.github.com>
2022-10-05 17:10:50 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('.menu-icon');
+
+menu.onclick = () =>{
+    menu.classList.toggle("move")
+};
+
+//Review swiper
+var swiper = new Swiper(".reviews-content", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
